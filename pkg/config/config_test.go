25dfcf4097940573ea5f4d0a6f918efd3a81587b
broderick.westrope <broderick.westrope@neds.com>
2024-02-28 17:38:12 +1100

    test: add TestParseJson & TestParseYaml

---
+++ pkg/config/config_test.go
@@ -0,0 +1,126 @@
+package config
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseJson(t *testing.T) {
+	type input struct {
+		data []byte
+	}
+	type expected struct {
+		cfg *Config
+		err bool
+	}
+
+	tt := []struct {
+		name     string
+		input    input
+		expected expected
+	}{
+		{
+			name: "TestParseYaml",
+			input: input{
+				data: []byte("{\"enums\": [{\"package\": \"validation\",\"type\": \"status\",\"values\": [\"Failed\",\"Passed\",\"Skipped\",\"Scheduled\",\"Running\"]}]}"),
+			},
+			expected: expected{
+				cfg: &Config{
+					OutputPath: "",
+					Configs: []*EnumConfig{
+						{
+							Package: "validation",
+							Type:    "status",
+							Enums:   []string{"Failed", "Passed", "Skipped", "Scheduled", "Running"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg, err := parseJson(tc.input.data)
+
+			if err != nil {
+				if !tc.expected.err {
+					t.Errorf("Error: expected nil, got %v", err)
+					return
+				}
+			} else {
+				if tc.expected.err {
+					t.Errorf("Error: expected error, got nil")
+					return
+				}
+			}
+
+			if cfg.OutputPath != tc.expected.cfg.OutputPath {
+				t.Errorf("Output Path: expected %v, got %v", tc.expected.cfg.OutputPath, cfg.OutputPath)
+			}
+			if !reflect.DeepEqual(cfg.Configs, tc.expected.cfg.Configs) {
+				t.Errorf("Enum Configs: expected %v, got %v", tc.expected.cfg.Configs, cfg.Configs)
+			}
+		})
+	}
+}
+
+func TestParseYaml(t *testing.T) {
+	type input struct {
+		data []byte
+	}
+	type expected struct {
+		cfg *Config
+		err bool
+	}
+
+	tt := []struct {
+		name     string
+		input    input
+		expected expected
+	}{
+		{
+			name: "TestParseYaml",
+			input: input{
+				data: []byte("enums:\n  - package: validation\n    type: status\n    values:\n      - Failed\n      - Passed\n      - Skipped\n      - Scheduled\n      - Running"),
+			},
+			expected: expected{
+				cfg: &Config{
+					OutputPath: "",
+					Configs: []*EnumConfig{
+						{
+							Package: "validation",
+							Type:    "status",
+							Enums:   []string{"Failed", "Passed", "Skipped", "Scheduled", "Running"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg, err := parseYaml(tc.input.data)
+
+			if err != nil {
+				if !tc.expected.err {
+					t.Errorf("Error: expected nil, got %v", err)
+					return
+				}
+			} else {
+				if tc.expected.err {
+					t.Errorf("Error: expected error, got nil")
+					return
+				}
+			}
+
+			if cfg.OutputPath != tc.expected.cfg.OutputPath {
+				t.Errorf("Output Path: expected %v, got %v", tc.expected.cfg.OutputPath, cfg.OutputPath)
+			}
+			if !reflect.DeepEqual(cfg.Configs, tc.expected.cfg.Configs) {
+				t.Errorf("Enum Configs: expected %v, got %v", tc.expected.cfg.Configs, cfg.Configs)
+			}
+		})
+	}
+}
